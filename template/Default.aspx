1ff1d8f47d0d1a6293f92d262c66510555278672
Wesley Hearn <whearn@redhat.com>
2013-08-28 15:53:17 -0400

    ASPX branch

---
+++ template/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Language="C#"%>
+<html>
+<head>
+<title>Hello World from ASPX on OpenShift!</title>
+</head>
+<body>
+  <% for (int i=1; i<=6; i++) {
+   Response.Write("<h" + i + ">Hello World!</h" + i + ">");
+   } %>
+</body>
+</html>
---
+++ template/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MySite_ASPX")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MySite_ASPX")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("893064ac-c092-4576-a5ff-1971930a1049")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
